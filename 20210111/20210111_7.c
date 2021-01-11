112de260a204c937796e2e9ecee8d400cf833231
Siyana Hristova <sisi2009_@abv.bg>
2021-01-11 20:17:44 +0200

    Add files via upload

---
+++ 20210111/20210111_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void obarniBitNa(unsigned int *n, int index);
+
+int main(void) { 
+    unsigned int n = 6;
+    obarniBitNa(&n, 2);
+    printf("%d\n", n);
+    return 0;
+}
+
+void obarniBitNa(unsigned int *n, int index){
+    *n ^= (1u << index);
+    unsigned mask = ~(1 << index);
+    *n = *n & mask;
+}
