0a2e7acabfb5368cf6616d46a7c92adbfd50375e
Siyana Hristova <sisi2009_@abv.bg>
2021-01-12 16:26:02 +0200

    Update 20210111_3.c

--- 20210111/20210111_3.c
+++ 20210111/20210111_3.c
@@ -1,3 +1,5 @@
+// Създайте функция iz4istiBitna. Тя получава като параметър число и индекс, и прави на нула бита на съответния индекс в числото
+
 #include <stdio.h>
 
 void iz4istiBitna(unsigned int *n, int index);
