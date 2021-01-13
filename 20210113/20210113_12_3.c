62a65c1cfd1fbf70bf2528474db4d249cc252e52
Siyana Hristova <sisi2009_@abv.bg>
2021-01-13 15:10:25 +0200

    Add files via upload

---
+++ 20210113/20210113_12_3.c
@@ -0,0 +1,18 @@
+// Напишете програма PrintPatterns, която отпечата следните текстови графики на екрана. Графиките една под друга
+#include <stdio.h> 
+
+int main(){
+    printf("          #          \n");
+    printf("        # # #        \n");  
+    printf("      # # # # #      \n");
+    printf("    # # # # # # #    \n");
+    printf("  # # # # # # # # #  \n");
+    printf("# # # # # # # # # # #\n");
+    printf("  # # # # # # # # #  \n");
+    printf("    # # # # # # #    \n");
+    printf("      # # # # #      \n");
+    printf("        # # #        \n");
+    printf("          #          \n");
+
+    return 0;
+}
