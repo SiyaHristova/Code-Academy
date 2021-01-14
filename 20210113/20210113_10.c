f7e4ebfffb296d6aa0ab3f4c73c692c4f9219ca0
Siyana Hristova <sisi2009_@abv.bg>
2021-01-14 10:06:39 +0200

    Add files via upload

---
+++ 20210113/20210113_10.c
@@ -0,0 +1,14 @@
+// Създайте С програма, която калкулира стойността на покупки в магазин за хранителни стоки. 
+// Клиентът може да закупи различна комбинация от продукти. В магазина има следните налични продукти и цени
+#include <stdio.h> 
+
+int main(){
+    unsigned int broiKaravani, broiKemperi;
+    int cenaDenKaravana = 90, cenaDenKemper = 100, rez;
+
+    printf("Dobre doshli v Karavani Kempeti pod naem\n");
+    printf("Ako zelaete karavana natisnete 1\n");
+    printf("Ako zelaete kemper natisnete 2\n");
+    scanf("%d", &rez);
+    return 0;
+}
