4854da13b6af9981531d9199273b1143bc9c6d88
Siyana Hristova <sisi2009_@abv.bg>
2021-01-14 10:17:59 +0200

    Update 20210113_8.c

--- 20210113/20210113_8.c
+++ 20210113/20210113_8.c
@@ -1,14 +1,17 @@
-#include <stdio.h> 
+// Да прочетем какво се случва със стойността на променливата x:
 
-int main(){
+#include <stdio.h>
+
+int main()
+{
     char x = 8;
     x = x -2;
-    printf("%d\n", x);
-
-    x = x + 6;
-    printf("%d\n", x);
-
+    printf("%c   %d \n",x,x);
+    
+    x= x+ 6;
+    printf("%c   %d \n",x,x);
+    
     x = x - 10 + 2;
-    printf("%d\n", x);
+    printf("%c   %d \n",x,x);
     return 0;
 }
