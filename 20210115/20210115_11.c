e2e0071b6e2c35044e2090ce9db49dac520ccddf
Siyana Hristova <sisi2009_@abv.bg>
2021-01-15 21:40:30 +0200

    Update 20210115_11.c

--- 20210115/20210115_11.c
+++ 20210115/20210115_11.c
@@ -2,9 +2,15 @@
 // . Ако имаме символен низ от символа char str[] = “Hello”;
 // копирайте всеки един от неговите символи в низ, който да изглежда
 // огледален на него. Ето така char rts[] = “ollaH”; и принтирайте резултата
+
 #include <stdio.h>
 int main() {
-    char str[] = "Hello";
-    printf("Hello: %c%c%c%c%c%c", str[4], str[3], str[2], str[1], str[0]);
+    char rts[] = " ", str[] = "Hello";
+    rts[0] = str[4];
+    rts[1] = str[3];
+    rts[2] = str[2];
+    rts[3] = str[1];
+    rts[4] = str[0];
+    printf("Hello: %s", rts);
     return 0;
 }
