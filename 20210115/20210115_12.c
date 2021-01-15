43a80703e4ab4b486422102d41cc62f43a764cbb
Siyana Hristova <sisi2009_@abv.bg>
2021-01-15 22:00:14 +0200

    Update 20210115_12.c

--- 20210115/20210115_12.c
+++ 20210115/20210115_12.c
@@ -1,9 +1,10 @@
 // Ако имаме символен низ char str[] = “Hеllo”, заменете буквите l с х на същия символен низ и отпечатайте резултата на конзолата.
+
 #include <stdio.h>
 int main() {
     char str[] = "Hello";
     str[2] = 'x';
     str[3] = 'x';
-    printf("Hello: %c%c%c%c%c%c", str[0], str[1], str[2], str[3], str[4]);
+    printf("Hello: %s", str);
     return 0;
 }
