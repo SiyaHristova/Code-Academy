2fc00968bce4327a757d975376bfc9b8c74c5869
Siyana Hristova <siyah.hristova@gmail.com>
2021-01-25 16:33:20 +0200

    Add files via upload

---
+++ 20210125/20210125_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int counter = 0;
+    int c;
+    c = getchar();
+    while (c!=EOF)
+    {
+        c = getchar();
+        counter ++;
+    }
+    printf("\n%d", counter);
+    return 0;
+}
