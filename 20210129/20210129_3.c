e2798585a2beb130e199d3bd2d3be9dc9cef4de3
Siyana Hristova <siyah.hristova@gmail.com>
2021-01-30 00:22:14 +0200

    Add files via upload

---
+++ 20210129/20210129_3.c
@@ -0,0 +1,20 @@
+/*  Напишете функция, която връща резултат (а+в)*4. */
+
+#include <stdio.h>
+
+int sum(int a, int b);
+int main(void)
+{
+    int a = 0, b = 0;
+    printf("a: ");
+    scanf("%d", &a);
+
+    printf("b: ");
+    scanf("%d", &b);
+    printf("%d\n", sum(a, b));
+    return 0;
+}
+
+int sum(int a, int b){
+    return (a+b)*4;
+}
