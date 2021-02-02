eedfe0dbedd6bb0ab48344fe8697ecbd1abeb5ae
Siyana Hristova <siyah.hristova@gmail.com>
2021-02-02 10:22:51 +0200

    Create 20210201_8.c

---
+++ 20210201/20210201_8.c
@@ -0,0 +1,21 @@
+/*  Напишете функция void reverse(char s[]), която обръща низа char s[
+]. Използвайте for цикъл с два аргумента, разделени със запетая
+i = 0, j = strlen(s) – 1; i < j; i++, j-- , за да обърнете стринга */
+
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *s){
+    int c, i, j;
+    for(i = 0, j = strlen(s) - 1; i<j; i++, j--){
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+int main(void){
+    char str[] = "String";
+    reverse(str);
+    printf("%s\n", str);
+    return 0;
+}
