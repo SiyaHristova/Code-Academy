6b0d49f199cdcb690b6600ecabd673fb9086d2c6
Siyana Hristova <siyah.hristova@gmail.com>
2021-02-18 15:24:41 +0200

    Update 20210215_12.c

--- 20210215/20210215_12.c
+++ 20210215/20210215_12.c
@@ -1,3 +1,25 @@
 /* Напишете програма, която дефинира масив [10][10][10],
 пълни го със случайни числа и смята средното аритметично,
 само с един цикъл. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+    srand((unsigned)time(0));
+    int arr[10][10][10];
+    int i, j, k;
+    unsigned int sum = 0;
+    
+    int *ip_arr = NULL;
+    ip_arr = (int*)arr;
+    for (i = 0; i < 10*10*10; i++){
+        *ip_arr = rand() % 101;
+        sum += *ip_arr++;
+    }
+    
+    int average = sum / (10*10*10);
+    printf("Average sum of elements: %i\n", average);
+    
+    return 0;
+}
