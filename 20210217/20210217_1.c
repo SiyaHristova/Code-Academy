23b7e264dd918350e6edc4cfe740492bb36f6064
Siyana Hristova <siyah.hristova@gmail.com>
2021-02-18 15:17:32 +0200

    Update 20210217_1.c

--- 20210217/20210217_1.c
+++ 20210217/20210217_1.c
@@ -4,31 +4,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int fnPlus(int nA, int nB){ 
-    return (nA + nB); 
-}
-int fnMinus(int nA, int nB){
-    return (nA - nB);
-}
-
-int main(int argc, char *argv[]){
-
+int fnPlus(int nA, int nB) { return (nA + nB); }
+int fnMinus(int nA, int nB) { return (nA - nB); }
+int main(int argc, char *argv[])
+{
     int (*pfCalc)(int, int) = NULL;
-
-    if (argc < 4){
+    if (argc < 4)
+    {
         printf("\nUsage: %s +/- arg1 arg2\n", argv[0]);
     }
-
-    else if ('-' == argv[1][0]){
-        pfCalc = &fnMinus;
+    else if ('-' == argv[1][0])
+    {
+        pfCalc = fnMinus;
     }
-
-    else if ('+' == argv[1][0]){
-        pfCalc = &fnPlus;
+    else if ('+' == argv[1][0])
+    {
+        pfCalc = fnPlus;
     }
-
-    (NULL == pfCalc) ? 0 : printf("\nResult: %d\n", (*pfCalc)(atoi(argv[1]), atoi(argv[3])));
-    
+    (NULL == pfCalc) ? 0 : printf("\nResult: %d\n", (*pfCalc)(atoi(argv[2]), atoi(argv[3])));
     return 0;
 }
