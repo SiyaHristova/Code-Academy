9eecfedd6c2479e9f3be9a9a38430ee6fce9fa6a
Siyana Hristova <siyah.hristova@gmail.com>
2021-02-21 17:48:33 +0200

    Update 20210217_7.c

--- 20210217/20210217_7.c
+++ 20210217/20210217_7.c
@@ -6,45 +6,42 @@
 
 int stringlength(char *s)
 {
-     int j;
-     for(j=0;s[j];j++);
-	 
-	 return j;
+	int j;
+	for (j = 0; s[j]; j++)
+		;
+	return j;
 }
 void printfrequencyofcharacters(char *s)
 {
-	int i,j,count=0,n;
- 
-    n=stringlength(s); 
-    
+	int i, j, count = 0, n;
+
+	n = stringlength(s);
+
 	printf(" frequency count character in string:\n");
- 
-    for(i=0;i<n;i++)  
-    {
-     	count=1;
-    	if(s[i])
-    	{
-		
- 		  for(j=i+1;j<n;j++)  
-	      {   
-	    	
-	        if(s[i]==s[j])
-    	    {
-                 count++;
-                 s[j]='\0';
-	     	}
-	      }  
-	       	      
-			printf(" '%c' = %d \n",s[i],count);
- 
-        }
- 	}  	 
+
+	for (i = 0; i < n; i++)
+	{
+		count = 1;
+		if (s[i])
+		{
+			for (j = i + 1; j < n; j++)
+			{
+				if (s[i] == s[j])
+				{
+					count++;
+					s[j] = '\0';
+				}
+			}
+			printf(" '%c' = %d \n", s[i], count);
+		}
+	}
 }
 int main()
 {
-    char s[1000];
-    printf("Enter  the string : ");
-    gets(s);
-    printfrequencyofcharacters(s);
+	char s[1000];
+	printf("Enter  the string : ");
+	gets(s);
+	printfrequencyofcharacters(s);
 
 	return 0;
+}
